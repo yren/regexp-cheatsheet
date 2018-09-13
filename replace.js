f664fdc6b647e8702ea727e62adbfa9add976b76
Yufei Ren [@yufei.ren] <yufei.ren@thomsonreuters.com>
2018-09-13 13:48:42 +0800

    update

--- replace.js
+++ replace.js
@@ -1,9 +1,28 @@
-// match Jack only when fo
+/*
+    /pattern/flags
+    new RegExp(pattern[, flags])
+*/
+var regex1 = /\w+/;
+var regex2 = new RegExp('\\w+');
+
+console.log(regex1);
+console.log(regex2);
+console.log(regex1 == regex2);
+
+// match Jack only when follow by 'Forest'
 var re = /Jack(?= Forest)/;
 var str = "I'm Jack Forest, you know that";
 var newstr = str.replace(re, "Tom");
 console.log(newstr);
 
-var str2 = "This Jack Neil, I don't know him.";
-var newstr2 = str2.replace(re, "Tom");
-console.log(newstr2);
+// match Tom only when NOT follow by 'Hanks'
+re = /Tom(?!= Hanks)/;
+str = "Tom More is a great player.";
+newstr = str.replace(re, "Jim");
+console.log(newstr);
+
+// change data format use Regexp
+re = /(\w+)\s(\w+)/;
+str = "Jim Simth";
+newstr = str.replace(re, '$2 $1');
+console.log(newstr);
